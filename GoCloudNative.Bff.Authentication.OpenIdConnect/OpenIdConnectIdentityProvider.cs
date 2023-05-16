3c1d5d257a832b3f4b8283b2b045af8c2114d550
Albert Starreveld <astarreveld@vxcompany.com>
2023-05-16 08:39:21 +0200

    refactor: Replaced Microsoft.Identity.Client with IdentityModel.OidcClient to retrieve the refreshtoken

--- GoCloudNative.Bff.Authentication.OpenIdConnect/OpenIdConnectIdentityProvider.cs
+++ GoCloudNative.Bff.Authentication.OpenIdConnect/OpenIdConnectIdentityProvider.cs
@@ -1,10 +1,12 @@
 using System.Net;
-using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Identity.Client;
 using GoCloudNative.Bff.Authentication.IdentityProviders;
+using IdentityModel;
+using IdentityModel.Client;
+using IdentityModel.OidcClient;
+using TokenResponse = GoCloudNative.Bff.Authentication.IdentityProviders.TokenResponse;
 
 namespace TheCloudNativeWebApp.Bff.Authentication.OpenIdConnect;
 
@@ -21,32 +23,58 @@ public class OpenIdConnectIdentityProvider : IIdentityProvider
     }
     
     public virtual async Task<AuthorizeRequest> GetAuthorizeUrlAsync(HttpContext context)
-    {   
-        var appToAuthorize = CreateConfidentialClientApplication(_configuration, context);
-        
-        var authorizeUrl = await appToAuthorize.GetAuthorizationRequestUrl(_configuration.Scopes)
-            .WithPkce(out var verifier)
-            .ExecuteAsync();
+    { 
+        var protocol = context.Request.IsHttps ? "https://" : "http://";
+        var redirectUrl = $"{protocol}{context.Request.Host}/oidc/login/callback";
 
-        return new AuthorizeRequest(new Uri(authorizeUrl.ToString()), verifier);
+        var client = new OidcClient(new OidcClientOptions
+        {
+            Authority = _configuration.Authority,
+            ClientId = _configuration.ClientId,
+            ClientSecret = _configuration.ClientSecret,
+            RedirectUri = redirectUrl,
+            Scope = string.Join(' ', _configuration.Scopes)
+        });
+
+        var request = await client.PrepareLoginAsync();
+        
+        return new AuthorizeRequest(new Uri(request.StartUrl), request.CodeVerifier);
     }
 
     public virtual async Task<TokenResponse> GetTokenAsync(HttpContext context, string code, string? codeVerifier)
     {
-        var appToAuthorize = CreateConfidentialClientApplication(_configuration, context);
+        var protocol = context.Request.IsHttps ? "https://" : "http://";
+        var redirectUrl = $"{protocol}{context.Request.Host}/oidc/login/callback";
 
-        var token = await appToAuthorize
-            .AcquireTokenByAuthorizationCode(_configuration.Scopes, code)
-            .WithPkceCodeVerifier(codeVerifier)
-            .ExecuteAsync();
+        _httpClient.BaseAddress = new Uri("https://localhost:7031/connect/token");
         
-        return new TokenResponse(token.AccessToken, token.IdToken, null);
+        var response = await _httpClient.RequestTokenAsync(new AuthorizationCodeTokenRequest
+        {
+             GrantType = OidcConstants.GrantTypes.AuthorizationCode,
+             ClientId = _configuration.ClientId,
+             ClientSecret = _configuration.ClientSecret,
+             
+             Parameters =
+             {
+                 { "code", code },
+                 { "scope", string.Join(' ', _configuration.Scopes) },
+                 { "redirect_uri", redirectUrl },
+                 { "code_verifier", codeVerifier },
+             }
+        });
+
+        if (response.IsError)
+        {
+            throw new ApplicationException($"Unable to retrieve token. OIDC server responded: {response.Error}");
+        }
+
+        return new TokenResponse(response.AccessToken, response.IdentityToken, response.RefreshToken);
     }
 
     public virtual async Task Revoke(string accessToken)
     {
         var openIdConfiguration = await GetWellKnownConfiguration();
-
+        
         var requestText = $"token={accessToken}" +
                           $"&client_id={_configuration.ClientId}" +
                           $"&client_secret={_configuration.ClientSecret}";
@@ -71,18 +99,5 @@ public class OpenIdConnectIdentityProvider : IIdentityProvider
         
         var httpResponse = await _httpClient.GetAsync(endpointAddress);
         return await httpResponse.Content.ReadFromJsonAsync<OpenIdConfiguration>();
-    }
-
-    private static IConfidentialClientApplication CreateConfidentialClientApplication(OpenIdConnectConfig config, HttpContext httpContext)
-    {   
-        var protocol = httpContext.Request.IsHttps ? "https://" : "http://";
-        var redirectUrl = $"{protocol}{httpContext.Request.Host}/oidc/login/callback";
-        
-        return ConfidentialClientApplicationBuilder.Create(config.ClientId)
-            .WithExperimentalFeatures()
-            .WithClientSecret(config.ClientSecret)
-            .WithGenericAuthority(config.Authority)
-            .WithRedirectUri(redirectUrl)
-            .Build();
     }
 }
