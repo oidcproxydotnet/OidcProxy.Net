0ef282969a3ed77c24f61abe159824e9d589bb6d
Albert Starreveld <astarreveld@vxcompany.com>
2023-05-22 13:03:22 +0200

    merge

---
+++ GoCloudNative.Bff.Authentication.Tests/JwtParserTests.cs
@@ -0,0 +1,6 @@
+namespace GoCloudNative.Bff.Authentication.Tests;
+
+public class JwtParserTests
+{
+    
+}
--- GoCloudNative.Bff.Authentication/JwtParser.cs
+++ GoCloudNative.Bff.Authentication/JwtParser.cs
@@ -1,26 +1,36 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Text;
-using System.Text.RegularExpressions;
 
 namespace GoCloudNative.Bff.Authentication;
 
-internal static class JwtParser
+public static class JwtParser
 {   
     public static JwtPayload ParseJwtPayload(this string token)
     {
-        var middleSection = GetSection(token, 2);
-        var json = Encoding.UTF8.GetString(Convert.FromBase64String(middleSection));
+        var middleSection = GetSection(token, 1);
+        byte[] bytes;
+
+        try
+        {
+            bytes = Convert.FromBase64String(middleSection);
+        }
+        catch (FormatException)
+        {
+            bytes = Convert.FromBase64String($"{middleSection}==");
+        }
+        
+        var json = Encoding.UTF8.GetString(bytes);
         return JwtPayload.Deserialize(json);
     }
 
     private static string GetSection(string token, int section)
     {
-        var match = Regex.Match(token, @"(\w*).(\w*).(\w*)");
-        if (!match.Success)
+        var chunks = token.Split(".", StringSplitOptions.None);
+        if (chunks.Length != 3)
         {
             throw new NotSupportedException($"Invalid token: {token}");
         }
 
-        return match.Groups[section].Value;
+        return chunks[section];
     }
 }
