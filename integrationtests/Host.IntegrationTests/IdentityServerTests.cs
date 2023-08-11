45fdb985cd299af5192d57dee5d64dd574a257ef
Albert Starreveld <albert@Alberts-MacBook-Pro.local>
2023-08-11 10:29:24 +0200

    refactor: Updated folder structure to match the work that needs doing on the project

---
+++ integrationtests/Host.IntegrationTests/IdentityServerTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Host.IntegrationTests;
+
+public class IdentityServerTests
+{
+    [Fact]
+    public async Task ItShouldWork()
+    {
+        Assert.True(true);
+    }
+}
