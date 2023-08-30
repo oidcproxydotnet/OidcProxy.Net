f6907954c948956c98444aa35a43c23a55521425
Albert Starreveld <albert@Alberts-MacBook-Pro.local>
2023-08-30 09:24:12 +0200

    feat(demos): Implemented call to /account/me endpoint

--- docs/demos/spa-bffs/auth0/src/spa+bff/ClientApp/src/app/app.module.ts
+++ docs/demos/spa-bffs/auth0/src/spa+bff/ClientApp/src/app/app.module.ts
@@ -5,17 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
     HomeComponent,
-    CounterComponent,
     FetchDataComponent
   ],
   imports: [
@@ -23,9 +19,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: '', component: HomeComponent, pathMatch: 'full' }
     ])
   ],
   providers: [],
--- docs/demos/spa-bffs/auth0/src/spa+bff/ClientApp/src/app/home/home.component.ts
+++ docs/demos/spa-bffs/auth0/src/spa+bff/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  user: any = { message: 'not signed in' };
+
+  constructor(private http: HttpClient) {
+    http.get("/account/me").subscribe((r) => {
+      this.user = r;
+    })
+  }
 }
