6d716b2c45188e72f3a246ac24e279c3dd036ae0
Albert Starreveld <albert@Alberts-MacBook-Pro.local>
2023-09-15 21:03:42 +0200

    fix: Updated unit tests to be compatible with new version of Microsoft.Identity.Jwt

--- unittests/GoCloudNative.Bff.Authentication.Tests/TokenFactoryTests.cs
+++ unittests/GoCloudNative.Bff.Authentication.Tests/TokenFactoryTests.cs
@@ -1,5 +1,3 @@
-using System.Text;
-using System.Text.RegularExpressions;
 using FluentAssertions;
 using GoCloudNative.Bff.Authentication.OpenIdConnect;
 
@@ -8,8 +6,8 @@ namespace GoCloudNative.Bff.Authentication.Tests;
 public class TokenFactoryTests
 {
     [Theory]
-    [InlineData("x.eydzdWInOiAnZTcxNjk1ODUtNDQ2YS00OTM4LWE3ODMtZTM0MDBiZTQwNmNiYzg4NWQzMGMtZmFjMi00NGZjLTlmMmEtMzMxOWYyZjNjM2Y5ZDA2NmM2MzQtY2RlZC00MDgzLWIyNDQtNmJlZDc1ZDAnIH0.x")]
-    [InlineData("x.eydzdWInOiAnM2JkOGRkZWYtMDlmZS00YTc4LTk2ZjMtNDgxNWNlM2FhYWU1NWVkZmE5OTYtMjcyZi00MGZlLTg3MGQtM2U4MDUyYjNhNGQ4NzdlMDI3MGItMzc4Yi00MmQ0LTg0OTktZjQ0ODhlMScgfQ.x")]
+    [InlineData("x.ewogICJzdWIiOiAiMjIzNDU2NzgiLAogICJuYW1lIjogIkphbmUgRG9lIiwKICAiaWF0IjogMTUxNjIzOTAyCn0.x")]
+    [InlineData("x.ewogICJzdWIiOiAiMjIzNDU2NzEyIiwKICAibmFtZSI6ICJKb2huIERvZSIsCiAgImlhdCI6IDE1MTYyMzkwMjEKfQ.x")]
     public void WhenPayloadShouldContainPadding_ShouldDecodePayload(string token)
     {
         var actual = token.ParseJwtPayload();
@@ -18,7 +16,7 @@ public class TokenFactoryTests
     }
 
     [Theory]
-    [InlineData("x.eydzdWInOiAnMmZiYTA4N2MtOTUxOS00MjBhLWEzMTItYjU1MWFjOTZmMGI4M2FiNGY0YTItN2VmZS00Y2M1LTgxNTUtZjkyMzQwZTU5YTA2ODFjMWYwNmUtMjM0MS00OWRiLTlmY2EtZjJlOTY2JyB9.x")]
+    [InlineData("x.ewogICJzdWIiOiAiMTIzNDU2NzgiLAogICJuYW1lIjogIkpvaG4gRG9lIiwKICAiaWF0IjogMTUxNjIzOTAyMgp9.x")]
     public void WhenPayloadShouldNotContainPadding_ShouldDecodePayload(string token)
     {
         var actual = token.ParseJwtPayload();
