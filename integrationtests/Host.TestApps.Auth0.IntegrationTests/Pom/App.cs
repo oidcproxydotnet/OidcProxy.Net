383dd153b04fe06a4d12413a97d873b00cd0a4a2
Albert Starreveld <albert@Alberts-MacBook-Pro.local>
2023-09-25 21:32:21 +0200

    fix: Fixt auth0 test

--- integrationtests/Host.TestApps.Auth0.IntegrationTests/Pom/App.cs
+++ integrationtests/Host.TestApps.Auth0.IntegrationTests/Pom/App.cs
@@ -16,7 +16,7 @@ public class App
 
         _browser = await Puppeteer.LaunchAsync(new LaunchOptions
         {
-            Headless = false,
+            Headless = true,
             IgnoreHTTPSErrors = true,
             Args = new []{ "--no-sandbox" }
         });
