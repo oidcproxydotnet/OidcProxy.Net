74840e592749985e36139cc9b8f86fa12e94ceb0
Albert Starreveld <albert@Alberts-MacBook-Pro.local>
2024-07-30 09:50:20 +0200

    feat(bootstrapping): Implemented bootstrapping overload

--- src/OidcProxy.Net.OpenIdConnect/ModuleInitializer.cs
+++ src/OidcProxy.Net.OpenIdConnect/ModuleInitializer.cs
@@ -11,15 +11,28 @@ public static class ModuleInitializer
     /// <param name="serviceCollection">An instance of the IServiceCollection, used to register the classes OidcProxy.Net uses internally.</param>
     /// <param name="config">An object containing the values from AppSettings.config</param>
     /// <param name="configureOptions">A lambda, used to configure OidcProxy.</param>
-    /// <returns>Used for chaining: Returns the instance of IServiceCollection.</returns>
-    public static IServiceCollection AddOidcProxy(this IServiceCollection serviceCollection, 
+    /// <returns>For chaining purposes: Returns the instance of IServiceCollection.</returns>
+    public static IServiceCollection AddOidcProxy(this IServiceCollection serviceCollection,
         OidcProxyConfig config,
-        Action<ProxyOptions>? configureOptions = null)
+        Action<ProxyOptions>? configureOptions = null) =>
+        AddOidcProxy<OpenIdConnectIdentityProvider>(serviceCollection, config, configureOptions);
+
+    /// <summary>
+    /// Initializes the proxy.
+    /// </summary>
+    /// <param name="serviceCollection">An instance of the IServiceCollection, used to register the classes OidcProxy.Net uses internally.</param>
+    /// <param name="config">An object containing the values from AppSettings.config</param>
+    /// <param name="configureOptions">A lambda, used to configure OidcProxy.</param>
+    /// <typeparam name="TIdentityProvider">A type reference to the concrete implementation of IIdentityProvider. This class implements everything that is needed to obtain tokens.</typeparam>
+    /// <returns>For chaining purposes: Returns the instance of IServiceCollection.</returns>
+    public static IServiceCollection AddOidcProxy<TIdentityProvider>(this IServiceCollection serviceCollection, 
+        OidcProxyConfig config,
+        Action<ProxyOptions>? configureOptions = null) where TIdentityProvider : OpenIdConnectIdentityProvider
     {
         if (config == null)
         {
             throw new ArgumentNullException(nameof(config), "Failed to initialise OidcProxy.Net. Config cannot be null. " +
-                $"Invoke `builder.Services.AddOidcProxy(..)` with an instance of `{nameof(OidcProxyConfig)}`.");
+                                                            $"Invoke `builder.Services.AddOidcProxy(..)` with an instance of `{nameof(OidcProxyConfig)}`.");
         }
         
         if (config.Oidc == null)
@@ -36,7 +49,7 @@ public static class ModuleInitializer
         
         serviceCollection.AddSingleton(config.Oidc);
         
-        return serviceCollection.AddOidcProxy<OpenIdConnectIdentityProvider>(options =>
+        return serviceCollection.AddOidcProxy<TIdentityProvider>(options =>
         {
             config.Apply(options);
             configureOptions?.Invoke(options);
